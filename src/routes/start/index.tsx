07b21190a667328944bd8220a11991a454cd7aaf
Dominik Fretz <dominik.fretz@gmail.com>
2024-06-02 22:58:07 -0500

    Fix for small screen

--- src/routes/start/index.tsx
+++ src/routes/start/index.tsx
@@ -9,12 +9,12 @@ const hover = `${transform} hover:scale-110`
 const cell = ``
 
 const Cell: React.FC<{ bgColor: string, mirrored: boolean, text: string, link: string, bgImage: string, bgAlt: string }> = ({ bgColor, mirrored, link, text, bgAlt, bgImage }) => {
-  return (<div className={`${bgColor} ${cell} w-1/2 h-screen flex relative`}>
+  return (<div className={`${bgColor} ${cell} w-full lg:w-1/2 h-screen flex relative`}>
     <img src={bgImage} alt={bgAlt} className="object-cover object-center h-full w-full" />
     <div className="absolute inset-0 bg-[#22211E] opacity-70"></div>
     <div className="absolute inset-0 flex justify-center items-center bg-black bg-opacity-50">
       <Link to={link} className={`relative ${hover} cursor-pointer flex ${mirrored ? 'flex-row-reverse' : ''} `}>
-        <Frame mirrored={mirrored} className={`${mirrored ? 'mr-[-3rem]' : 'ml-[-2rem]'} lg:h-[7.75rem] md:h-[5.25rem]`} />
+        <Frame mirrored={mirrored} className={`${mirrored ? 'mr-[-3rem]' : 'ml-[-2rem]'} h-[4.5rem] lg:h-[7.75rem] md:h-[5.25rem]`} />
         <div className={`text-white font-bold w-full relative  text-5xl  md:text-6xl lg:text-8xl pt-2`}>
           {text}
         </div>
