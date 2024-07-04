c240b20ce7c6ee0d5c2cab4a236b7efa8c16cc9d
Dominik Fretz <dominik.fretz@gmail.com>
2024-07-03 22:39:57 -0500

    Move to hash router

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,24 +1,19 @@
 
 import { createElement } from 'react';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import { createHashRouter, RouterProvider } from 'react-router-dom';
 import { BaseLayout, MainLayout, RootError } from '../components';
 
 /**
  * Application routes
  * https://reactrouter.com/en/main/routers/create-browser-router
  */
-export const router = createBrowserRouter([
+export const router = createHashRouter([
   {
     path: '',
     element: <BaseLayout />,
     errorElement: <RootError />,
     children: [
       { index: true, lazy: () => import('./start') },
-      // { path: 'start', lazy: () => import('./start') },
-
-      // { path: 'login', lazy: () => import('./login') },
-      // { path: 'privacy', lazy: () => import('./privacy') },
-      // { path: 'terms', lazy: () => import('./terms') },
     ],
   },
   {
@@ -26,11 +21,8 @@ export const router = createBrowserRouter([
     element: <MainLayout />,
     errorElement: <RootError />,
     children: [
-      // { index: true, element: <Navigate to="/start" replace /> },
       { path: 'coding', lazy: () => import('./coding') },
       { path: 'photo', lazy: () => import('./photo') },
-      // { path: 'tasks', lazy: () => import('./tasks') },
-      // { path: 'messages', lazy: () => import('./messages') },
     ],
   },
 ]);
